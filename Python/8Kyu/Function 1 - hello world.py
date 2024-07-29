16ad5ef195ad8b0d314f684800bf418faa32c1db
SabrinaLynnDev <sabrinalynn010101@gmail.com>
2024-07-29 10:20:39 -0400

    Create Function 1 - hello world.py

---
+++ Python/8Kyu/Function 1 - hello world.py
@@ -0,0 +1,8 @@
+#Function 1 - hello world
+
+
+#Make a simple function called greet that returns the most-famous "hello world!".
+
+# Write a function `greet` that returns "hello world!"
+def greet():
+    return "hello world!"
