8a1ff8ab5715097ab511d20d8bb22b775fdfc12d
SabrinaLynnDev <sabrinalynn010101@gmail.com>
2025-01-05 14:48:59 -0500

    Update Find the first non-consecutive number.js

--- JS/8kyu/Find the first non-consecutive number.js
+++ JS/8kyu/Find the first non-consecutive number.js
@@ -1,5 +1,4 @@
-/*
-Find the first non-consecutive number
+/*Find the first non-consecutive number
 
 Description:
 Your task is to find the first element of an array that is not consecutive.
@@ -10,11 +9,15 @@ E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all con
 
 If the whole array is consecutive then return null2.
 
-The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+The array will always have at least 2 elements1 and all elements will be numbers. 
+The numbers will also all be unique and in ascending order. 
+The numbers could be positive or negative and the first non-consecutive could be either too!
 
 If you like this Kata, maybe try this one next: https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
 
-1 Can you write a solution that will return null2 for both [] and [ x ] though? (This is an empty array and one with a single number and is not tested for, but you can write your own example test. )
+1 Can you write a solution that will return null2 for both [] and [ x ] though? 
+(This is an empty array and one with a single number and is not tested for, 
+but you can write your own example test. )
 
 2
 Swift, Ruby and Crystal: nil
@@ -25,3 +28,6 @@ Nim: none(int) (See options)
 
  */
 
+
+//prep 
+//p arr-> min 2 ints
