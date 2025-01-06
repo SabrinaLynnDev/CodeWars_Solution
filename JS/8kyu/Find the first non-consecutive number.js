f9f724221840eeacaf84d39fb382f3805b6f8b6f
SabrinaLynnDev <sabrinalynn010101@gmail.com>
2025-01-06 10:41:10 -0500

    Update Find the first non-consecutive number.js

--- JS/8kyu/Find the first non-consecutive number.js
+++ JS/8kyu/Find the first non-consecutive number.js
@@ -29,5 +29,11 @@ Nim: none(int) (See options)
  */
 
 
-//prep 
-//p arr-> min 2 ints
+function firstNonConsecutive(arr) {
+    for (let i = 0; i < arr.length - 1; ++i) {
+      if (arr[i] + 1 !== arr[i + 1]) {
+        return arr[i + 1]
+      }
+    }
+    return null
+  }
